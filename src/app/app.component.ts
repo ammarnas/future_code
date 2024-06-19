e8693a15bf16c0420db3d7a69cfad34aea9a5b85
ammarnas <ammarnasser.contact@gmail.com>
2024-06-19 12:36:22 +0300

    use @for

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CourseCardComponent } from "./course-card/course-card.component";
 import { ICourse } from './app.component.models';
-import { NgForOf } from '@angular/common';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [RouterOutlet, CourseCardComponent, NgForOf]
+    imports: [RouterOutlet, CourseCardComponent]
 })
 export class AppComponent {
   data = {
