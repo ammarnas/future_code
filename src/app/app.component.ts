9771b455c6d302e2bfc4f9e651389c94a8124806
ammarnas <ammarnasser.contact@gmail.com>
2024-06-20 17:08:43 +0300

    example of ngClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CourseCardComponent } from "./course-card/course-card.component";
-import { ICourse } from './app.component.models';
+import { CategoryType, ICourse } from './app.component.models';
 
 @Component({
     selector: 'app-root',
@@ -20,7 +20,7 @@ export class AppComponent {
     title: 'Angular For Beginners',
     description:'However, one should not forget further development of various forms of activity directly depends on new propositions. The significance of such problems is so obvious that the implementation of the planned tasks plays an important part in the formation of the step-by-step consistent development of the society. The diverse and plentiful experience constant quantitive growth and the scope of our activity directly depends on the step-by-step consistent development of the society.',
     lessonsCount: 89,
-    category: 1,
+    category: CategoryType.beginners,
     longDescription: '',
     imageUrl: 'https://picsum.photos/id/684/200/200',
   },
@@ -29,7 +29,7 @@ export class AppComponent {
     title: 'Rxjs',
     description:'However, one should not forget the innovational path that we have chosen contributes to the preparing and implementation of further ways of development. On the other hand constant quantitive growth and the scope of our activity requires the clarification of the system of mass participation. On the other hand further development of various forms of activity contributes to increasing the quality of existing financial and administrative conditions.',
     lessonsCount: 25,
-    category: 2,
+    category: CategoryType.advanced,
     longDescription: '',
     imageUrl: 'https://picsum.photos/id/685/200/200',
   },
@@ -38,7 +38,7 @@ export class AppComponent {
     title: 'Angular Material UI',
     description:'Likewise hi-tech concept of the social system allows to appreciate the value of economic practicability of decisions made. The significance of such problems is so obvious that the innovational path that we have chosen directly depends on strengthening the democratic system.',
     lessonsCount: 33,
-    category: 2,
+    category: CategoryType.advanced,
     longDescription: '',
     imageUrl: 'https://picsum.photos/id/557/200/200',
   },
@@ -47,7 +47,7 @@ export class AppComponent {
     title: 'Course With no image',
     description:'Likewise hi-tech concept of the social system allows to appreciate the value of economic practicability of decisions made. The significance of such problems is so obvious that the innovational path that we have chosen directly depends on strengthening the democratic system.',
     lessonsCount: 33,
-    category: 2,
+    category: CategoryType.beginners,
     longDescription: '',
     imageUrl: 'https://picsum.photos/id/556/200/200',
   }
