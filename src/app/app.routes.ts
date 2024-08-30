8165393822a29df94fde45ece47ba4498dee4a6b
ammarnas <ammarnasser.contact@gmail.com>
2024-08-30 15:27:57 +0300

    #33 add Redirect routes and Wildcard route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CourseListComponent } from './course-list/course-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {
@@ -10,5 +11,17 @@ export const routes: Routes = [
   {
     path: 'course-list',
     component: CourseListComponent
+  },
+  {
+    //Redirect routes
+    path: '',
+    pathMatch: 'full',
+    redirectTo: "home"
+  },
+  {
+    //Wildcard notation Pages
+    // Note: Wildcard route should always placed at last
+    path: "**",
+    component: PageNotFoundComponent
   }
 ];
