e144bc8b30693633906d15ff35da34fa3c334dfc
ammarnas <ammarnasser.contact@gmail.com>
2024-08-30 08:44:05 +0300

    add header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -8,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+  data = {
+    courseName: 'Angular Core Course!',
+    lessonName: 'Angular For Beginners In Arabic',
+  };
+  isNavbarSticky = false;
 }
