18b05f661285c9edd330f1204deb1b761d487dfe
ammarnas <ammarnasser.contact@gmail.com>
2024-08-31 08:48:27 +0300

    #34 add backgroundColor method with no using

--- src/app/course-card/course-card.component.ts
+++ src/app/course-card/course-card.component.ts
@@ -12,10 +12,25 @@ import { ChildViewComponent } from '../child-view/child-view.component';
 })
 export class CourseCardComponent {
   @Input({required: true}) course: ICourse = {} as ICourse;
-  @Output() viewCourseEvent = new EventEmitter<ICourse>();
-
   categoryType = CategoryType;
-  viewCourse(): void {
-    this.viewCourseEvent.emit(this.course);
+
+  get backgroundColor(): string {
+    const defaultColor = "#fa7901";
+    if(this.course) {
+      switch(this.course.category) {
+        case this.categoryType.intermediate:
+          return "#00adee";
+        case this.categoryType.advanced:
+          return "green";
+        case this.categoryType.other:
+          return "#b4bd00";
+        default:
+          return defaultColor;
+      }
+    }
+    else {
+      return defaultColor;
+    }
   }
+  viewCourse(): void {}
 }
