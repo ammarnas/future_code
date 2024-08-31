a0d65cc5bbc351e204e15f0ad44102f6606e32a0
ammarnas <ammarnasser.contact@gmail.com>
2024-08-31 09:26:47 +0300

    #35 get the route parameters by static way

--- src/app/course-details/course-details.component.ts
+++ src/app/course-details/course-details.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ICourse } from '../app.component.models';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-course-details',
@@ -10,4 +11,20 @@ import { ICourse } from '../app.component.models';
 })
 export class CourseDetailsComponent {
   course!: ICourse;
+
+  constructor(private activatedRoute: ActivatedRoute){}
+
+// Note:
+// 1- Not recommend it
+// 2- Not detect the changes of the route
+  ngOnInit() {
+  // first static way
+  const id = this.activatedRoute.snapshot.params['id'];
+  console.log('snapshot params', id);
+
+  // second static way
+  const courseId = this.activatedRoute.snapshot.paramMap.get('id');
+  console.log('snapshot paramMap', courseId);
+
+  }
 }
