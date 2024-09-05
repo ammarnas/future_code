bd82914cc1c8f4e5a29298b39520598d6edd115f
ammarnas <ammarnasser.contact@gmail.com>
2024-09-05 22:03:23 +0300

    #39- Understanding Angular Nesting Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { HomeComponent } from './home/home.component';
 import { CourseListComponent } from './course-list/course-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { CourseDetailsComponent } from './course-details/course-details.component';
+import { AccountComponent } from './account/account.component';
+import { LoginComponent } from './account/login/login.component';
+import { RegisterComponent } from './account/register/register.component';
 
 export const routes: Routes = [
   {
@@ -20,6 +23,15 @@ export const routes: Routes = [
   {
     path: 'course-list/:id',
     component: CourseDetailsComponent
+  },
+  {
+    path: 'account',
+    component: AccountComponent,
+    children: [
+      {path: 'login', component: LoginComponent },
+      {path: 'register', component: RegisterComponent },
+      {path: '', redirectTo: '/account/login', pathMatch: 'full'}
+    ]
   },
   {
     //Redirect routes
