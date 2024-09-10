d4574f12d2931577dee3b81b6c182b23f7b15bef
ammarnas <ammarnasser.contact@gmail.com>
2024-09-10 21:52:59 +0300

    #43 Handle request errors

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { baseURL } from '../../environment/environment';
-import { Observable } from 'rxjs';
+import { catchError, elementAt, Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +10,16 @@ export class ApiService {
   http = inject(HttpClient);
 
   getRequest<T>(name: string) : Observable<T> {
-    return this.http.get<T>(`${baseURL}${name}/`);
+    return this.http.get<T>(`${baseURL}${name}/`)
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorResponse: HttpErrorResponse): Observable<any> {
+    if(errorResponse.status === 0){
+      console.log(`A client side error occurred: ${errorResponse.status} - ${errorResponse.error}`);
+    } else {
+      console.log(`A back end side error occurred: ${errorResponse.status} - ${errorResponse.error}`);
+    }
+    return throwError(() => new Error("Something went wrong, pleas try again later"));
   }
 }
