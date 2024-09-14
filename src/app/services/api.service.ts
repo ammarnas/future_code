347c2491bbdc8e9200bb88e6fbdf253a05a40407
ammarnas <ammarnasser.contact@gmail.com>
2024-09-14 09:53:03 +0300

    #45 create handle error service and move handle error to it

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,24 +2,18 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { baseURL } from '../../environment/environment';
 import { catchError, elementAt, Observable, throwError } from 'rxjs';
+import { HandleErrorService } from './handle-error.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
   http = inject(HttpClient);
+  handleErrorService = inject(HandleErrorService);
 
   getRequest<T>(name: string) : Observable<T> {
     return this.http.get<T>(`${baseURL}${name}/`)
-      .pipe(catchError(this.handleError));
-  }
-
-  private handleError(errorResponse: HttpErrorResponse): Observable<any> {
-    if(errorResponse.status === 0){
-      console.log(`A client side error occurred: ${errorResponse.status} - ${errorResponse.error}`);
-    } else {
-      console.log(`A back end side error occurred: ${errorResponse.status} - ${errorResponse.error}`);
-    }
-    return throwError(() => new Error("Something went wrong, pleas try again later"));
+      // .pipe(
+      //   catchError(this.handleErrorService.handleError));
   }
 }
---
+++ src/app/services/handle-error.service.ts
@@ -0,0 +1,18 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HandleErrorService {
+
+  public handleError(errorResponse: HttpErrorResponse): Observable<any> {
+    if(errorResponse.status === 0){
+      console.log(`A client side error occurred: ${errorResponse.status} - ${errorResponse.error}`);
+    } else {
+      console.log(`A back end side error occurred: ${errorResponse.status} - ${errorResponse.error}`);
+    }
+    return throwError(() => new Error("Something went wrong, pleas try again later"));
+  }
+}
