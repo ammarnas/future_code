cf180d40dbde9f3ff7b90a56aea06698604ca354
ammarnas <ammarnasser.contact@gmail.com>
2024-09-14 15:13:01 +0300

    style film card

--- src/app/film-card/film-card.component.ts
+++ src/app/film-card/film-card.component.ts
@@ -12,32 +12,39 @@ import { Router } from '@angular/router';
   styleUrl: './film-card.component.scss'
 })
 export class FilmCardComponent {
-  @Input({required: true}) film: IFilm = {} as IFilm;
+  @Input({required: true}) film!: IFilm;
   categoryType = CategoryType;
+  randomImage!: string;
 
   constructor(private router: Router) {
+    this.getRandomImage();
   }
-  // get backgroundColor(): string {
-  //   const defaultColor = "#fa7901";
-  //   // if(this.film) {
-  //   //   switch(this.film.category) {
-  //   //     case this.categoryType.intermediate:
-  //   //       return "#00adee";
-  //   //     case this.categoryType.advanced:
-  //   //       return "green";
-  //   //     case this.categoryType.other:
-  //   //       return "#b4bd00";
-  //   //     default:
-  //   //       return defaultColor;
-  //   //   }
-  //   // }
-  //   // else {
-  //   //   return defaultColor;
-  //   // }
-  // }
+  get backgroundColor(): string {
+    const defaultColor = "#fa7901";
+    if(this.film) {
+      switch(this.film.episode_id) {
+        case 1:
+          return "#00adee";
+        case 2:
+          return "green";
+        case 3:
+          return "#b4bd00";
+        default:
+          return defaultColor;
+      }
+    }
+    else {
+      return defaultColor;
+    }
+  }
   viewFilm(): void {
     // this.router.navigateByUrl(`film-list/${this.film.id}`);
+    console.log(this.film.id)
+    this.router.navigate(['film'], {queryParams: {id: this.film.id}});
+  }
 
-    this.router.navigate(["film", {queryParams: {id: 5, name: "Test"}}]);
+  getRandomImage() {
+    let random = Math.floor(Math.random() * 1000);
+    this.randomImage = `https://picsum.photos/id/${random}/1000/1000`;
   }
 }
