03382c80345de0863bae2d763a59b59a86e63f8e
ammarnas <ammarnasser.contact@gmail.com>
2024-10-12 13:10:01 +0300

    implement register component

--- src/app/account/register/register.component.ts
+++ src/app/account/register/register.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+import { NavigationService } from '../../shared/services/navigation.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [SharedModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
 
+  hide = true;
+  hideConfirmPassword = true;
+  private navigationService = inject(NavigationService);
+
+  navigateToLogin(): void {
+    this.navigationService.navigateByUrl('/account/login');
+  }
 }
