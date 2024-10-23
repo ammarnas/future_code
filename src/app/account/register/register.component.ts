6039290475a1b4f457ca52ec088a6375d4f49b17
ammarnas <ammarnasser.contact@gmail.com>
2024-10-23 21:23:17 +0300

    #60- Introduction to Angular Reactive Form

--- src/app/account/register/register.component.ts
+++ src/app/account/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { NavigationService } from '../../shared/services/navigation.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -14,8 +15,22 @@ export class RegisterComponent {
   hide = true;
   hideConfirmPassword = true;
   private navigationService = inject(NavigationService);
+  registerForm: FormGroup
 
-  
+  constructor() {
+    this.registerForm = this.initFrom();
+  }
+
+  private initFrom() : FormGroup {
+    return new FormGroup({
+      userName: new FormControl(''),
+      email: new FormControl(''),
+      mobileNumber: new FormControl(''),
+      password: new FormControl(''),
+      confirmPassword: new FormControl(''),
+    });
+  }
+
   navigateToLogin(): void {
     this.navigationService.navigateByUrl('/account/login');
   }
